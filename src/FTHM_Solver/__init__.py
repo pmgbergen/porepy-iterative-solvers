7fd3bd9b4c5fd717f9c23ca6a83c874233d47e46
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-04-09 13:18:42 +0000

    MAINT: Update of top-level init

--- src/FTHM_Solver/__init__.py
+++ src/FTHM_Solver/__init__.py
@@ -1,12 +1,20 @@
-from . import block_matrix, full_petsc_solver, mat_utils, iterative_solver
+from . import (
+    block_matrix,
+    full_petsc_solver,
+    mat_utils,
+    iterative_solver,
+    fluid_mass_balance_solver,
+)
 
 from .block_matrix import *
 from .full_petsc_solver import *
 from .mat_utils import *
 from .iterative_solver import *
+from .fluid_mass_balance_solver import *
 
 __all__ = []
 __all__.extend(full_petsc_solver.__all__)
 __all__.extend(block_matrix.__all__)
 __all__.extend(mat_utils.__all__)
 __all__.extend(iterative_solver.__all__)
+__all__.extend(fluid_mass_balance_solver.__all__)
