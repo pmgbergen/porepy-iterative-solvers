38e1e65b9aa24080961f43d064f49b4d766dfd5c
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-06-24 07:56:02 +0000

    BLD: Fixes to top-level init

--- src/FTHM_Solver/__init__.py
+++ src/FTHM_Solver/__init__.py
@@ -1,12 +1,12 @@
 version = "0.0.1"
 
 from .block_matrix import *
-from .iterative_solver import *
+from .solver_mixin import *
 from .mat_utils import *
 from .preconditioners import *
 
 __all__ = []
 __all__.extend(block_matrix.__all__)
 __all__.extend(mat_utils.__all__)
-__all__.extend(iterative_solver.__all__)
+__all__.extend(solver_mixin.__all__)
 __all__.extend(preconditioners.__all__)
