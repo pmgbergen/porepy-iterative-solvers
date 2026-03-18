e3754ad265e1f3bb8a7ee322562dea0976f31bab
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2026-03-18 10:37:32 +0000

    STY: Isort, ruff

--- src/pp_solvers/__init__.py
+++ src/pp_solvers/__init__.py
@@ -3,9 +3,9 @@ version = "0.0.1"
 from .block_linear_system import *
 from .mat_utils import *
 from .petsc_utils import *
+from .plot_linear_system import *
 from .preconditioners import *
 from .solver_mixin import *
-from .plot_linear_system import *
 
 __all__ = []
 __all__.extend(block_linear_system.__all__)
