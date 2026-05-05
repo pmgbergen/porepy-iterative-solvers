024e41064ced5d7f1aad40b5feeb8389fbc6991b
Your Name <you@example.com>
2026-05-05 09:42:35 +0000

    MAINT: Improved importing of solver selector

---
+++ src/pp_solvers/solver_selection/__init__.py
@@ -0,0 +1,11 @@
+from .solver_space import CategoricalChoices, NumericalChoices, SolverSpace
+from .selector import SolverSelector
+from .performance_predictor import assemble_default_performance_predictor
+
+__all__ = [
+    "CategoricalChoices",
+    "NumericalChoices",
+    "SolverSpace",
+    "SolverSelector",
+    "assemble_default_performance_predictor",
+]
