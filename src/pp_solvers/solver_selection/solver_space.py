6fbb101d86b9c283f60d6ca43512c73cf3154eae
Yury Zabegaev <zabegaev99@gmail.com>
2026-05-21 10:13:40 +0000

    DOC: added docstring to `_str`

--- src/pp_solvers/solver_selection/solver_space.py
+++ src/pp_solvers/solver_selection/solver_space.py
@@ -457,9 +457,9 @@ class _DecisionNode:
 
         self.id: int = -1  # This will be set in _initialize_decision_ids.
 
-    def string(self, prefix: str = "") -> str:
-        """Human-readible representation of the node and its children for debug and
-        analysis.
+    def _str(self, prefix: str = "") -> str:
+        """A recursive method that should be called from `__str__`. This traverses the
+        tree and appends its children representation to the prefix.
 
         """
         if not isinstance(self.solver_space_scheme, dict):
@@ -474,7 +474,7 @@ class _DecisionNode:
             tmp = "\n".join(numerical_repr)
             repr = f"{repr}\n{tmp}"
         if len(self.children) > 0:
-            child_repr = [child.string(prefix=child_prefix) for child in self.children]
+            child_repr = [child._str(prefix=child_prefix) for child in self.children]
             tmp = "\n".join(child_repr)
             repr = f"{repr}\n{tmp}"
         return repr
@@ -487,7 +487,11 @@ class _DecisionNode:
         return f"DecisionNode({self.solver_space_scheme})"
 
     def __str__(self) -> str:
-        return self.string()
+        """Human-readible representation of the node and its children for debug and
+        analysis.
+
+        """
+        return self._str()
 
     def list_possible_solvers(self) -> list[_FlatCompleteSolverConfig]:
         """Generates a list of all complete configurations defined by this solver space.
@@ -540,14 +544,22 @@ class _ForkNode:
         return f"ForkNode({self.options_key}, id={self.id})"
 
     def __str__(self) -> str:
-        return self.string()
+        """Human-readible representation of the node and its children for debug and
+        analysis.
 
-    def string(self, prefix: str = "") -> str:
+        """
+        return self._str()
+
+    def _str(self, prefix: str = "") -> str:
+        """A recursive method that should be called from `__str__`. This traverses the
+        tree and appends its children representation to the prefix.
+
+        """
         num = len(self.categorical_choices.choices)
         repr = f"{prefix}{self.options_key} (fork with {num} branches):"
         child_prefix = f"{prefix}| "
         if len(self.children) > 0:
-            child_repr = [child.string(prefix=child_prefix) for child in self.children]
+            child_repr = [child._str(prefix=child_prefix) for child in self.children]
             tmp = "\n".join(child_repr)
             repr = f"{repr}\n{tmp}"
         return repr
